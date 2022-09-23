09a344c765915ae593bc2cfaa9ad72576cede40d
unknown <hifalmuhammad43@gmail.com>
2022-09-22 21:09:39 -0700

    revisi2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,6 +9,7 @@
 
             <h1>Halaman about</h1>
             <h3>{{ $nama }}</h3>
+            <p>{{ $bio }}</p>
             <p>{{ $email }}</p>
             <img src="img/{{ $image }}" alt="burung1.jpg" width="400">
 
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('/about', function () {
     return view('about', [
         "title" => "about",
         "nama" => "Muhammad Hifallussalam",
+        "bio" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perferendis, repudiandae nobis autem quas quia placeat minus, minima, facere error quasi sapiente voluptates ducimus officiis iure vitae possimus deleniti alias. Molestiae repellendus id, commodi deserunt quis cumque eaque esse magnam a magni atque ipsum iste odit doloribus! Inventore, praesentium mollitia?",
         "email" => "hifalmuhammad43@gmail.com",
         "image" => "burung1.jpg"
 
