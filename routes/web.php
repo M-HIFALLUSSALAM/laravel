b673c2694cd5527a9fa74a9e3698eb42482dfc53
unknown <hifalmuhammad43@gmail.com>
2022-10-13 22:11:22 -0700

    revisi6

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,4 @@ Route::get('/about', function () {
 
 Route::get('/blog', [PostControler::class, 'index']);
 
-Route::get('posts/{post}', [PostControler::class, 'show']);
+Route::get('posts/{post:slug}', [PostControler::class, 'show']);
