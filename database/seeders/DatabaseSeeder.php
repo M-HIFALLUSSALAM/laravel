0ded0302bd80ea3620332e8f9123423d152717b5
unknown <hifalmuhammad43@gmail.com>
2022-10-20 21:18:39 -0700

    revisi9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\category;
+use App\Models\post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,48 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'salam',
+            'email' => 'salam@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        category::create([
+            'name' => 'web programming',
+            'slug' => 'web-programming'
+        ]);
+
+        category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        post::create([
+            'title' => 'judul pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil. Expedita ea vel provident. Ab numquam, temporibus nam, odio obcaecati, provident odit in eaque nobis maiores pariatur natus! Reiciendis sunt sint ipsam quo veniam eos explicabo quos fugiat nobis, consectetur expedita et doloremque veritatis atque eius sequi asperiores exercitationem! Non pariatur ea vel officia esse, nemo recusandae laboriosam. Temporibus, alias. Dolor omnis itaque nostrum illum? Fuga nobis iste dolor iusto, corporis voluptatibus amet quisquam. Error, dolorem veritatis, vero asperiores vitae sit dignissimos id pariatur ducimus amet temporibus hic doloribus neque. Sit atque itaque accusantium.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        post::create([
+            'title' => 'judul kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil. Expedita ea vel provident. Ab numquam, temporibus nam, odio obcaecati, provident odit in eaque nobis maiores pariatur natus! Reiciendis sunt sint ipsam quo veniam eos explicabo quos fugiat nobis, consectetur expedita et doloremque veritatis atque eius sequi asperiores exercitationem! Non pariatur ea vel officia esse, nemo recusandae laboriosam. Temporibus, alias. Dolor omnis itaque nostrum illum? Fuga nobis iste dolor iusto, corporis voluptatibus amet quisquam. Error, dolorem veritatis, vero asperiores vitae sit dignissimos id pariatur ducimus amet temporibus hic doloribus neque. Sit atque itaque accusantium.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        post::create([
+            'title' => 'judul ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil.',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit modi sequi fugiat quasi, soluta exercitationem nihil. Expedita ea vel provident. Ab numquam, temporibus nam, odio obcaecati, provident odit in eaque nobis maiores pariatur natus! Reiciendis sunt sint ipsam quo veniam eos explicabo quos fugiat nobis, consectetur expedita et doloremque veritatis atque eius sequi asperiores exercitationem! Non pariatur ea vel officia esse, nemo recusandae laboriosam. Temporibus, alias. Dolor omnis itaque nostrum illum? Fuga nobis iste dolor iusto, corporis voluptatibus amet quisquam. Error, dolorem veritatis, vero asperiores vitae sit dignissimos id pariatur ducimus amet temporibus hic doloribus neque. Sit atque itaque accusantium.',
+            'category_id' => 1,
+            'user_id' => 2
+        ]);
     }
 }
